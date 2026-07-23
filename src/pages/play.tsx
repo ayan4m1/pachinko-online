3f7d2bc996b85d9ca8435c582d9b3f666595005b
ayan4m1 <andrew@bulletlogic.com>
2026-07-22 20:37:49 -0400

    feat: add blank r3f Canvas

--- src/pages/play.tsx
+++ src/pages/play.tsx
@@ -1,8 +1,11 @@
+import { Canvas } from '@react-three/fiber';
+
 export const Component = () => {
   return (
     <div>
       <title>Play Online Pachinko</title>
       <h1>Play</h1>
+      <Canvas></Canvas>
     </div>
   );
 };
