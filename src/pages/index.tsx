6a16a8f72265152f2591cdf35b89c4856609c279
ayan4m1 <andrew@bulletlogic.com>
2026-07-22 20:37:33 -0400

    feat: add <title>s

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 export const Component = () => {
   return (
     <div>
+      <title>Online Pachinko</title>
       <h1>Online Pachinko</h1>
     </div>
   );
--- src/pages/play.tsx
+++ src/pages/play.tsx
@@ -1,6 +1,7 @@
 export const Component = () => {
   return (
     <div>
+      <title>Play Online Pachinko</title>
       <h1>Play</h1>
     </div>
   );
